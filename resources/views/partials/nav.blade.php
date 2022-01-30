c7a7a94e39922cb5e2063ff3be9d9868e57f11f8
Azril Nazli Alias <sales@stream.my>
2022-01-30 18:10:15 +0800

    nav link

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -2,7 +2,7 @@
     <div class="position-sticky pt-3">
       <ul class="nav flex-column">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/video">
+          <a class="nav-link active" aria-current="page" href="/">
             <span data-feather="home"></span>
             Dashboard
           </a>
